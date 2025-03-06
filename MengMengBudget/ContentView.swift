896f9dc2eef9d89ce17632639cbc640d48e1b34e
xuzm8 <xuzm8@asiainfo.com>
2025-03-06 15:42:48 +0800

    Initial Commit

---
+++ MengMengBudget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MengMengBudget
+//
+//  Created by 徐泽敏 on 2025/3/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MengMengBudget/MengMengBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MengMengBudgetApp.swift
+//  MengMengBudget
+//
+//  Created by 徐泽敏 on 2025/3/6.
+//
+
+import SwiftUI
+
+@main
+struct MengMengBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
